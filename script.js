625bc132f97375f0d2a94fe3bf34fa5ab44bd82f
Randy Douglas <58892815+RandyWritesCode@users.noreply.github.com>
2022-07-17 19:05:27 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+function verifyCounselorCode(event) {
+
+}
+
+const counselorId = document.getElementById("counselorCode")
+
+document.getElementById("counselorCode").submit = function () { verifyCounselorCode() };
+
+function verifyCounselorCode() {
+  console.log("help")
+  // alert('counselor verified')
+  // document.getElementById("myForm").className = ""
+}
+
+//https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit_event
